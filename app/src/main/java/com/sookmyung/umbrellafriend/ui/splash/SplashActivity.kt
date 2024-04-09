2aad96544dc6110be820b6c35dc24c2d76ad978e
stellar-halo <stellarhalo@naver.com>
2024-04-09 16:43:51 +0900

    #3 [ui] splash 화면 띄우기

---
+++ app/src/main/java/com/sookmyung/umbrellafriend/ui/splash/SplashActivity.kt
@@ -0,0 +1,44 @@
+package com.sookmyung.umbrellafriend.ui.splash
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import com.sookmyung.umbrellafriend.R
+import com.sookmyung.umbrellafriend.databinding.ActivitySplashBinding
+import com.sookmyung.umbrellafriend.util.binding.BindingActivity
+
+class SplashActivity : BindingActivity<ActivitySplashBinding>(R.layout.activity_splash) {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Handler(Looper.getMainLooper()).postDelayed(
+            {
+                startApp()
+//                업데이트 버전 관리 로직
+//                when (viewModel.isUpdate.value) {
+//                    UpdateType.NONE -> startApp()
+//                    UpdateType.FORCE -> showForceUpdateDialog()
+//                    UpdateType.RECOMMEND -> showRecommendUpdateDialog()
+//                    else -> {}
+//                }
+            }, SPLASH_DELAY
+        )
+    }
+
+    private fun startApp() {
+//        회원가입 여부에 따른 이동화면 전환
+//        val intent: Intent = if (viewModel.isSignedUp() && viewModel.isMember()) {
+//            Intent(this, MainActivity::class.java)
+//        } else {
+//            Intent(this, LoginActivity::class.java)
+//        }
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
+        finish()
+    }
+
+    companion object {
+        const val SPLASH_DELAY = 2000L
+    }
+}
