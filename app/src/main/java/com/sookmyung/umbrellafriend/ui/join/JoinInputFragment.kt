859241f3292812612e093847c7164a89778ece73
stellar-halo <stellarhalo@naver.com>
2024-04-10 17:56:53 +0900

    #5 [ui] join fragment,activity 작업(로직 없음)

---
+++ app/src/main/java/com/sookmyung/umbrellafriend/ui/join/JoinInputFragment.kt
@@ -0,0 +1,13 @@
+package com.sookmyung.umbrellafriend.ui.join
+
+import android.os.Bundle
+import android.view.View
+import com.sookmyung.umbrellafriend.R
+import com.sookmyung.umbrellafriend.databinding.FragmentJoinInfoBinding
+import com.sookmyung.umbrellafriend.util.binding.BindingFragment
+
+class JoinInputFragment : BindingFragment<FragmentJoinInfoBinding>(R.layout.fragment_join_info) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
