fb42fb5a5ed16a7afd51c3b8d8e4ba2b213707c5
stellar-halo <stellarhalo@naver.com>
2024-04-17 20:36:31 +0900

    #9 [ui] 홈 화면 ui 작업

--- app/src/main/java/com/sookmyung/umbrellafriend/ui/main/MainActivity.kt
+++ app/src/main/java/com/sookmyung/umbrellafriend/ui/main/MainActivity.kt
@@ -3,10 +3,12 @@ package com.sookmyung.umbrellafriend.ui.main
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.sookmyung.umbrellafriend.R
+import com.sookmyung.umbrellafriend.databinding.ActivityMainBinding
+import com.sookmyung.umbrellafriend.util.binding.BindingActivity
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MainActivity : AppCompatActivity() {
+class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
