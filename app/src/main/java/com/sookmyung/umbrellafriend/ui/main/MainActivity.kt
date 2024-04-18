6364fcade69789145546190565154d72ee6adfd3
stellar-halo <stellarhalo@naver.com>
2024-04-18 16:49:29 +0900

    #9 [ui] 홈 화면 연체, 대여, 미대여 상태에 따른 뷰 분기처리

--- app/src/main/java/com/sookmyung/umbrellafriend/ui/main/MainActivity.kt
+++ app/src/main/java/com/sookmyung/umbrellafriend/ui/main/MainActivity.kt
@@ -1,10 +1,19 @@
 package com.sookmyung.umbrellafriend.ui.main
 
-import androidx.appcompat.app.AppCompatActivity
+import android.graphics.Typeface
 import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.StyleSpan
+import android.text.style.UnderlineSpan
+import android.view.View
 import androidx.activity.viewModels
+import androidx.core.content.ContextCompat
 import com.sookmyung.umbrellafriend.R
 import com.sookmyung.umbrellafriend.databinding.ActivityMainBinding
+import com.sookmyung.umbrellafriend.domain.entity.RentalStatus.NOT_RENTED
+import com.sookmyung.umbrellafriend.domain.entity.RentalStatus.OVERDUE
+import com.sookmyung.umbrellafriend.domain.entity.RentalStatus.RENTING
 import com.sookmyung.umbrellafriend.util.binding.BindingActivity
 import dagger.hilt.android.AndroidEntryPoint
 
@@ -15,5 +24,79 @@ class MainActivity : BindingActivity<ActivityMainBinding>(R.layout.activity_main
         super.onCreate(savedInstanceState)
 
         binding.vm = viewModel
+
+        viewModel.home.observe(this) {
+            when (viewModel.rentalStatus) {
+                NOT_RENTED -> updateViewForNotRented()
+                RENTING -> updateViewForRenting()
+                OVERDUE -> updateViewForOverdue()
+            }
+        }
+    }
+
+    private fun updateViewForNotRented() {
+        updateReturnView(true, "사용한 우산을 반납해요")
+        updateRentalView("우산을 대여할 수 있어요", R.drawable.ic_unfold_umbrella)
+    }
+
+
+    private fun updateViewForRenting() {
+        updateReturnView(
+            false, "사용한 우산을 반납해요", R.color.main_blue, getString(
+                R.string.home_return_renting_date,
+                viewModel.home.value?.dDay?.daysRemaining.toString()
+            )
+        )
+        updateRentalView("3일 추가로 연장돼요", R.drawable.ic_unfold_umbrella_gray)
+        setSpannableBuilder()
+    }
+
+    private fun updateViewForOverdue() {
+        binding.clHomeUmbrellaRental.isClickable = false
+        updateReturnView(
+            false, "대여한 우산이 연체되었어요", R.color.sub_orange, getString(
+                R.string.home_return_overdue_date,
+                viewModel.home.value?.dDay?.daysRemaining.toString()
+            )
+        )
+        updateRentalView("연장할 수 없어요", R.drawable.ic_unfold_umbrella_gray)
+        setSpannableBuilder()
+    }
+
+    private fun updateReturnView(
+        iconVisible: Boolean,
+        subTitle: String,
+        dateColor: Int = R.color.main_blue,
+        dateText: String = ""
+    ) {
+        with(binding.tvHomeUmbrellaReturnDate) {
+            visibility = if (iconVisible) View.INVISIBLE else View.VISIBLE
+            text = dateText
+            setTextColor(ContextCompat.getColor(this@MainActivity, dateColor))
+        }
+
+        with(binding) {
+            ivHomeUmbrellaReturnIcon.visibility = if (iconVisible) View.VISIBLE else View.INVISIBLE
+            tvHomeUmbrellaReturnSubtitle.text = subTitle
+        }
+    }
+
+    private fun updateRentalView(subTitle: String, icon: Int) {
+        with(binding) {
+            tvHomeUmbrellaRentalSubtitle.text = subTitle
+            ivHomeUmbrellaRentalIcon.setImageResource(icon)
+        }
+    }
+
+    private fun setSpannableBuilder() {
+        val indexes = viewModel.findDateIndex()
+        if (indexes != null) {
+            val ssb = SpannableStringBuilder(viewModel.home.value?.weather?.message)
+            ssb.apply {
+                setSpan(StyleSpan(Typeface.BOLD), indexes.first, indexes.second, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+                setSpan(UnderlineSpan(), indexes.first, indexes.second, Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+            }
+            binding.tvHomeProfileMention.text = ssb
+        }
     }
 }
--- app/src/main/java/com/sookmyung/umbrellafriend/ui/main/MainViewModel.kt
+++ app/src/main/java/com/sookmyung/umbrellafriend/ui/main/MainViewModel.kt
@@ -6,6 +6,10 @@ import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.sookmyung.umbrellafriend.domain.entity.Home
+import com.sookmyung.umbrellafriend.domain.entity.RentalStatus
+import com.sookmyung.umbrellafriend.domain.entity.RentalStatus.NOT_RENTED
+import com.sookmyung.umbrellafriend.domain.entity.RentalStatus.OVERDUE
+import com.sookmyung.umbrellafriend.domain.entity.RentalStatus.RENTING
 import com.sookmyung.umbrellafriend.domain.usecase.GetHomeUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
@@ -18,6 +22,7 @@ class MainViewModel @Inject constructor(
 ) : ViewModel() {
     private val _home: MutableLiveData<Home> = MutableLiveData()
     val home: LiveData<Home> get() = _home
+    var rentalStatus: RentalStatus = NOT_RENTED
 
     init {
         getHome()
@@ -26,10 +31,42 @@ class MainViewModel @Inject constructor(
     private fun getHome() {
         viewModelScope.launch {
             getHomeUseCase().onSuccess { response ->
+                checkRentalStatus()
                 _home.value = response
             }.onFailure { throwable ->
                 Timber.e("$throwable")
             }
         }
     }
-}
+
+    private fun checkRentalStatus() {
+        val dDay = home.value?.dDay
+
+        rentalStatus = when {
+            dDay?.isOverDue == true -> OVERDUE
+            dDay?.overdueDays == -1 -> NOT_RENTED
+            else -> RENTING
+        }
+    }
+
+    fun findDateIndex(): Pair<Int, Int>? {
+        val text = home.value?.weather?.message
+        val charSeq: CharSequence = text ?: ""
+        val pattern = "\\b\\d+일\\b".toRegex()
+        val matchResult = pattern.find(charSeq)
+
+        return if (matchResult != null) {
+            val startIndex = matchResult.range.first
+            val endIndex = matchResult.range.last + 1
+            Timber.tag(HOME_TAG).d("'n일'의 인덱스: ($startIndex , $endIndex)")
+            Pair(startIndex, endIndex)
+        } else {
+            Timber.tag(HOME_TAG).e("'n일'을 찾을 수 없습니다.")
+            null
+        }
+    }
+
+    companion object {
+        const val HOME_TAG = "HOME"
+
+    }}
