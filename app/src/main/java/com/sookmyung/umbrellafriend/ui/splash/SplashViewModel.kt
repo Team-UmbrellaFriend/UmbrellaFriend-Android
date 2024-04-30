0d8036bb03e85410da802a32b9653b4ae3e8195a
stellar-halo <stellarhalo@naver.com>
2024-04-30 17:40:46 +0900

    #27 [feat] 버전 서버 연결

--- app/src/main/java/com/sookmyung/umbrellafriend/ui/splash/SplashViewModel.kt
+++ app/src/main/java/com/sookmyung/umbrellafriend/ui/splash/SplashViewModel.kt
@@ -1,21 +1,43 @@
 package com.sookmyung.umbrellafriend.ui.splash
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.sookmyung.umbrellafriend.domain.entity.Version
 import com.sookmyung.umbrellafriend.domain.usecase.GetLoginUseCase
+import com.sookmyung.umbrellafriend.domain.usecase.GetVersionUseCase
 import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.launch
+import timber.log.Timber
 import javax.inject.Inject
 
 @HiltViewModel
 class SplashViewModel @Inject constructor(
-    private val getLoginUseCase: GetLoginUseCase
+    private val getLoginUseCase: GetLoginUseCase,
+    private val getVersionUseCase: GetVersionUseCase
 ) : ViewModel() {
     private val _isLogin: MutableLiveData<Boolean> = MutableLiveData(false)
     val isLogin: LiveData<Boolean> get() = _isLogin
+    private val _version: MutableLiveData<Version> = MutableLiveData()
+    val version: LiveData<Version> get() = _version
 
     init {
+        getVersion()
         isLogin()
+    }
+
+    private fun getVersion() {
+        viewModelScope.launch {
+            getVersionUseCase()
+                .onSuccess { response ->
+                    _version.value = response
+                }
+                .onFailure { throwable ->
+                    Timber.e("$throwable")
+                }
+        }
     }
 
     private fun isLogin() {
